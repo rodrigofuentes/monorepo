eb305b8867409e3f14f85ce5edf4e64c311bd196
Rodrigo Fuentes <rodrigofuentes@users.noreply.github.com>
2022-10-31 16:23:51 -0400

    adds useIsomorphic

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useToggle } from './useToggle'
 export { default as useCompare } from './useCompare'
 export { default as useDebounce } from './useDebounce'
+export { default as useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect'
